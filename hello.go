1a1ef3b951ce67e0c26974f2df1d1d0274b9bd77
ChacaS0 <chk.chaca@gmail.com>
2018-11-14 11:31:04 +0400

    another change

--- hello.go
+++ hello.go
@@ -22,8 +22,7 @@ import (
 	"github.com/golang/example/stringutil"
 )
 
-func main() 
-{
+func main() {
 	fmt.Println(goreverse("!selpmaxe oG ,olleH"))
 	// fmt.Println(goreverse("Hello, Go example!"))
 }
